f5b074495daa5891ae608d3dad05f3a4ff0638b9
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-06-17 18:38:48 -0300

    poo - adicionando exemplos de data

---
+++ src/academy/javaoca/danieloliveira/classes_utilitarias/Rdatas/test/DataTest01.java
@@ -0,0 +1,10 @@
+package academy.javaoca.danieloliveira.classes_utilitarias.Rdatas.test;
+
+import java.util.Date;
+
+public class DataTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(1781730681300L);
+        System.out.println(date);
+    }
+}
