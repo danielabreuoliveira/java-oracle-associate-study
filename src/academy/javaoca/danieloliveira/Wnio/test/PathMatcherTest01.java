a05f51f78c52861abd0efbf7f94ab5b77f617a47
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-07-11 13:49:42 -0300

    Adicionando exemplos de NIO - PathMatcher

---
+++ src/academy/javaoca/danieloliveira/Wnio/test/PathMatcherTest01.java
@@ -0,0 +1,21 @@
+package academy.javaoca.danieloliveira.Wnio.test;
+
+import java.nio.file.*;
+
+public class PathMatcherTest01 {
+    public static void main(String[] args) {
+        Path path1 = Paths.get("pasta/subpasta/file.bkp");
+        Path path2 = Paths.get("pasta/subpasta/file.txt");
+        Path path3 = Paths.get("pasta/subpasta/file.java");
+        matches(path1, "glob:*.bkp");
+        matches(path1, "glob:**/*.bkp");
+        matches(path3, "glob:**/*.{bkp,txt,java}");
+        matches(path3, "glob:**/*.???");
+        matches(path3, "glob:**/file.???");
+        matches(path3, "glob:**/file.????");
+    }
+    private static void matches(Path path, String glob){
+        PathMatcher matcher = FileSystems.getDefault().getPathMatcher(glob);
+        System.out.println(glob + ": " + matcher.matches(path));
+    }
+}
