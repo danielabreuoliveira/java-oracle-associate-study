4719358be82eb096dc484ab52c09be860e8bab65
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-07-21 13:02:22 -0300

    adicionando exemplos de collections operador equals

--- src/academy/javaoca/danieloliveira/YColecoes/test/EqualsTest01.java
+++ src/academy/javaoca/danieloliveira/YColecoes/test/EqualsTest01.java
@@ -5,7 +5,7 @@ import academy.javaoca.danieloliveira.YColecoes.dominio.Smartphone;
 public class EqualsTest01 {
     public static void main(String[] args) {
         Smartphone s1 = new Smartphone("1ABC1", "iphone");
-        Smartphone s2 = s1;
+        Smartphone s2 = new Smartphone("1ABC1", "iphone");
         System.out.println(s1.equals(s2));
     }
 }
