42496c1846353abaf64687a317d193d68bd0cb3f
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-07-30 11:08:27 -0300

    leet code merge Strings

---
+++ src/academy/javaoca/danieloliveira/leetcode/exercicios02/Exercicios02Test.java
@@ -0,0 +1,14 @@
+package academy.javaoca.danieloliveira.leetcode.exercicios02;
+
+public class Exercicios02Test {
+    public static void main(String[] args) {
+
+        String word1 = ("abc");
+        String word2 = ("pqr");
+
+        Exercicios02 exercicios02 = new Exercicios02();
+        String resultado = exercicios02.mergeAlternately(word1, word2);
+        System.out.println(resultado);
+    }
+
+}
