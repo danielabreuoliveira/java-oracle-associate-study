2f9422d53c6e7b4ba32845b67c1d5371bb0326f6
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-08-06 09:01:00 -0300

    adicionando exemplo estrutura dados stringbuilder

---
+++ src/academy/javaoca/danieloliveira/leetcode/exercicios02/Solucao5.java
@@ -0,0 +1,27 @@
+package academy.javaoca.danieloliveira.leetcode.exercicios02;
+
+    public class Solucao5 {
+        public String mergeComStrings(String palavra1, String palavra2){
+            int p1 = palavra1.length();
+            int p2 = palavra2.length();
+
+            StringBuilder resultado = new StringBuilder();
+            int i = 0; int j = 0;
+
+            while (i<p1||j<p2){
+                if(i<p1){
+                    resultado.append(palavra1.charAt(i++) + "-");
+                }
+                if (j<p2){
+                    resultado.append(palavra2.charAt(j++));
+                    if(j<p1){
+                        resultado.append("-");
+                    }
+                }
+            }
+            return resultado.toString();
+        }
+    }
+
+
+
