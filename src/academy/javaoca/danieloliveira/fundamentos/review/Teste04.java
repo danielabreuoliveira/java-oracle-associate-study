2817d13af531ff77d9e63d233ff42f90052b39a2
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-08-15 22:01:05 -0300

    realizando exeercicios modularizacao e funcoes

--- src/academy/javaoca/danieloliveira/fundamentos/review/Teste04.java
+++ src/academy/javaoca/danieloliveira/fundamentos/review/Teste04.java
@@ -1,4 +1,18 @@
 package academy.javaoca.danieloliveira.fundamentos.review;
 
 public class Teste04 {
+    public double calcularFrete(double peso, double distancia){
+        return peso * distancia * 0.5;
+    }
+
+    public static void main(String[] args) {
+
+        Teste04 calcularFrete1 = new Teste04();
+        Teste04 calcularFrete2 = new Teste04();
+
+        double frete01 = calcularFrete1.calcularFrete(1.75, 10.0);
+        double frete02 = calcularFrete2.calcularFrete(1.0, 300);
+        System.out.println("Frete produto 1: " + frete01);
+        System.out.println("Frete produto 2: " + frete02);
+    }
 }
