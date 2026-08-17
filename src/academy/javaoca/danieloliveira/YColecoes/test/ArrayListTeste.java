83ef9ee7f423a6af0e8eaaba5a210b9f71a18c15
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-08-17 18:41:17 -0300

    pratiando arraylist

---
+++ src/academy/javaoca/danieloliveira/YColecoes/test/ArrayListTeste.java
@@ -0,0 +1,34 @@
+package academy.javaoca.danieloliveira.YColecoes.test;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTeste {
+    public static void main(String[] args) {
+
+
+        List<String> times = new ArrayList<>();
+
+        times.add("Flamengo");
+        times.add("Palmeiras");
+        times.add("Cruzeiro");
+        times.add("Mirassol");
+        times.add("Fluminense");
+        times.add("Bahia");
+        times.add("Botafogo");
+        times.add("São Paulo");
+        times.add("Red Bull Bragantino");
+        times.add("Corinthians");
+        times.add("Grêmio");
+        times.add("Vasco");
+        times.add("Atlético-MG");
+        times.add("Santos");
+        times.add("Vitória");
+        times.add("Internacional");
+        times.add("Coritiba");
+        times.add("Athletico-PR");
+        times.add("Chapecoense");
+        times.add("Remo");
+
+        System.out.println(times);
+    }
+}
