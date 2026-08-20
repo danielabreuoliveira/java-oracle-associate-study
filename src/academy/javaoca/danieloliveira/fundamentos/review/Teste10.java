fd0c5d285df8ee05c52fd58195f6c9b812da9327
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-08-20 18:26:38 -0300

    praticando arraylist

---
+++ src/academy/javaoca/danieloliveira/fundamentos/review/Teste10.java
@@ -0,0 +1,16 @@
+package academy.javaoca.danieloliveira.fundamentos.review;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Teste10 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numeros =  new ArrayList<>();
+
+        numeros.add(5);
+        numeros.add(11);
+
+        System.out.println("Lista: " + numeros);
+    }
+}
