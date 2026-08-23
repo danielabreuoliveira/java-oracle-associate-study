48a83ca4caee97fd674ba9341546f8902dd6a4b8
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-08-23 11:04:57 -0300

    exercicios de fundamentos

---
+++ src/academy/javaoca/danieloliveira/fundamentos/review/Teste22.java
@@ -0,0 +1,18 @@
+package academy.javaoca.danieloliveira.fundamentos.review;
+
+public class Teste22 {
+    public static void main(String[] args) {
+
+        String nome = "Daniel";
+        int idade = 35;
+        double salario = 1.500;
+        double altura = 1.71;
+        boolean aprovado = true;
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(salario);
+        System.out.println(altura);
+        System.out.println(aprovado);
+    }
+}
