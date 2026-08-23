debf0f405d0a4113119c191627d63ded3c83b481
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-08-22 23:04:17 -0300

    exercicios de fundamentos

--- src/academy/javaoca/danieloliveira/fundamentos/review/Teste13.java
+++ src/academy/javaoca/danieloliveira/fundamentos/review/Teste13.java
@@ -1,4 +1,20 @@
 package academy.javaoca.danieloliveira.fundamentos.review;
 
+import java.util.Scanner;
+
 public class Teste13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Idade digitada: " + idade + " anos");
+        System.out.println("---------------------------");
+        System.out.println("Pessoa pode votar: " + (idade>=16));
+        System.out.println("Pessoa pode tirar a caterira: " + (idade>=18));
+        System.out.println("Pessoa pode aposentar: " + (idade>=65));
+
+        scanner.close();
+    }
 }
