006a8f13f817f0894f59ee7441c7a39d177c32cf
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-08-27 15:07:11 -0300

    exercicios de fundamentos

---
+++ src/academy/javaoca/danieloliveira/fundamentos/review/Teste38.java
@@ -0,0 +1,25 @@
+package academy.javaoca.danieloliveira.fundamentos.review;
+
+public class Teste38 {
+    public static void main(String[] args) {
+                int[] numeros = {1, 2, 3, 4, 5};
+
+                int inicio = 0;
+                int fim = numeros.length - 1;
+
+                while (inicio < fim) {
+
+                    int temp = numeros[inicio];
+                    numeros[inicio] = numeros[fim];
+                    numeros[fim] = temp;
+
+                    inicio++;
+                    fim--;
+                }
+
+                for (int i = 0; i < numeros.length; i++) {
+                    System.out.println(numeros[i]);
+                }
+            }
+        }
+
