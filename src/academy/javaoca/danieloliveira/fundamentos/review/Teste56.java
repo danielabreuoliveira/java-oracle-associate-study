3f931f029a01db28c95ea2a2f2985364785f4006
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-08-29 14:10:19 -0300

    exercicios de fundamentos

---
+++ src/academy/javaoca/danieloliveira/fundamentos/review/Teste56.java
@@ -0,0 +1,14 @@
+package academy.javaoca.danieloliveira.fundamentos.review;
+
+public class Teste56 {
+    public static void main(String[] args) {
+        String nome = "42";
+        double numero = 9.0;
+
+        int converterString = Integer.parseInt(nome);
+        int coverterDouble = (int) numero;
+
+        System.out.println(converterString);
+        System.out.println(coverterDouble);
+    }
+}
