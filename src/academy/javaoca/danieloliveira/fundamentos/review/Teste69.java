6faf0d6de0a045787dcf984dcd6981984a7c0b9f
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-08-30 00:37:35 -0300

    exercicios de fundamentos

---
+++ src/academy/javaoca/danieloliveira/fundamentos/review/Teste69.java
@@ -0,0 +1,12 @@
+package academy.javaoca.danieloliveira.fundamentos.review;
+
+public class Teste69 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 30; i++) {
+            if(i%3==0){
+                System.out.println(i);
+                continue;
+            }
+        }
+    }
+}
