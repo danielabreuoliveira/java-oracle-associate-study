ae2546611c03d1dc165b3f0c78ebc2d42994b4b3
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-08-31 00:15:52 -0300

    exercicios de fundamentos

---
+++ src/academy/javaoca/danieloliveira/fundamentos/review/Teste82.java
@@ -0,0 +1,14 @@
+package academy.javaoca.danieloliveira.fundamentos.review;
+
+public class Teste82 {
+
+    public boolean ehPar(int numero){
+        return numero%2==0;
+    }
+    public static void main(String[] args) {
+
+        Teste82 teste82 = new Teste82();
+
+        System.out.println(teste82.ehPar(21));
+    }
+}
