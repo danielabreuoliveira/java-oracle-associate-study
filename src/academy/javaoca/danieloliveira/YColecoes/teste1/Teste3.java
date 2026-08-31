f1b4e803b081a8eb474ee03c40e6a406cf2d9018
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-08-31 18:50:31 -0300

    exercicios de fundamentos

---
+++ src/academy/javaoca/danieloliveira/YColecoes/teste1/Teste3.java
@@ -0,0 +1,25 @@
+package academy.javaoca.danieloliveira.YColecoes.teste1;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Teste3 {
+    public static void main(String[] args) {
+        List<Integer> numeros = new ArrayList<>();
+
+        numeros.add(2);
+        numeros.add(2);
+        numeros.add(3);
+        numeros.add(4);
+        numeros.add(1);
+        numeros.add(3);
+        numeros.add(7);
+
+        HashSet<Integer> numerosSemDuplicados = new HashSet<>(numeros);
+
+        System.out.println(numerosSemDuplicados);
+
+
+    }
+}
