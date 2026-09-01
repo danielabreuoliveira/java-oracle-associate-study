ee7cb580d6162130ac70c9dd83bb486e3f694d72
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-09-01 15:10:58 -0300

    exercicios de fundamentos

---
+++ src/academy/javaoca/danieloliveira/fundamentos/review/Teste93.java
@@ -0,0 +1,11 @@
+package academy.javaoca.danieloliveira.fundamentos.review;
+
+public class Teste93 {
+    public static void main(String[] args) {
+        int[] numeros = {1,33,123,11,2323,45,22};
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+    }
+}
