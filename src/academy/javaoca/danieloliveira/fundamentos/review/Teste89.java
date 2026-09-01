2bf38200851de00082dc12205b254ed814d1c6f0
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-09-01 11:34:47 -0300

    exercicios de fundamentos

---
+++ src/academy/javaoca/danieloliveira/fundamentos/review/Teste89.java
@@ -0,0 +1,12 @@
+package academy.javaoca.danieloliveira.fundamentos.review;
+
+public class Teste89 {
+    public static void main(String[] args) {
+        int somaTodosNumeros = 0;
+        for (int i = 1; i <= 1000; i++) {
+            somaTodosNumeros += i;
+        }
+
+        System.out.println(somaTodosNumeros);
+    }
+}
