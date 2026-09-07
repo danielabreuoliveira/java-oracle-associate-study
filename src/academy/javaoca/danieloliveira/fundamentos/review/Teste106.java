05692153b32bb971386b0827eb13aa8fde121c07
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-09-07 13:46:45 -0300

    exercicios de fundamentos

---
+++ src/academy/javaoca/danieloliveira/fundamentos/review/Teste106.java
@@ -0,0 +1,20 @@
+package academy.javaoca.danieloliveira.fundamentos.review;
+
+public class Teste106 {
+    public static void main(String[] args) {
+        int[] numeros = {1,2,2,3,3,1,1};
+
+        for (int i = 0; i < numeros.length; i++) {
+            boolean repetido = false;
+            for (int j = 0; j < i ; j++) {
+                if(numeros[i] == numeros[j]){
+                    repetido = true;
+                    break;
+                }
+            }
+            if(!repetido){
+                System.out.print(numeros[i] + " ");
+            }
+        }
+    }
+}
