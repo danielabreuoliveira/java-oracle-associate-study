39ac8d6c247fdd21fb101597580c0a72063088db
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-09-07 13:56:35 -0300

    exercicios de fundamentos

---
+++ src/academy/javaoca/danieloliveira/fundamentos/review/Teste108.java
@@ -0,0 +1,12 @@
+package academy.javaoca.danieloliveira.fundamentos.review;
+
+public class Teste108 {
+    public static void main(String[] args) {
+        String nome = "Daniel";
+
+        for (int i = 0; i < nome.length(); i++) {
+            System.out.println(nome.charAt(i));
+        }
+
+    }
+}
