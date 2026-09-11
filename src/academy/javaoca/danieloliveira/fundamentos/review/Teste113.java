4999f07b9b223112c06979837d455474c946affc
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-09-11 19:42:40 -0300

    exercicios de fundamentos

---
+++ src/academy/javaoca/danieloliveira/fundamentos/review/Teste113.java
@@ -0,0 +1,30 @@
+package academy.javaoca.danieloliveira.fundamentos.review;
+
+public class Teste113 {
+
+        static void main(String[] args) {
+            String nome = "ovo";
+
+            int inicio = 0;
+            int fim = nome.length()-1;
+
+            boolean palindrommo = true;
+
+            do{
+                if(nome.charAt(inicio)!=nome.charAt(fim)){
+                    palindrommo = false;
+                }
+                inicio++;
+                fim--;
+            }while (inicio<fim);
+
+            if(palindrommo){
+                System.out.println("A palavra: " + nome + " é palidromo");
+            }else {
+                System.out.println("A palavar " + nome + " não é palindromo");
+            }
+        }
+
+    }
+
+
